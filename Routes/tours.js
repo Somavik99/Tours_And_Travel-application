3e58fde05ac8142867e3d596223426cb149eb3d9
Avik Roy <somavik99@gmail.com>
2024-07-31 09:46:20 +0530

    Limit and skip updated

--- Routes/tours.js
+++ Routes/tours.js
@@ -1,12 +1,15 @@
-import express from 'express';
-import { authenticateToken } from '../Authentication/tokenVarification.js';
-import { getAllToursData, getSingleTourData, postToursLocation } from '../Controllers/toursController.js';
+import express from "express";
+import { authenticateToken } from "../Authentication/tokenVarification.js";
+import {
+  getAllToursData,
+  getSingleTourData,
+  postToursLocation,
+} from "../Controllers/toursController.js";
 
 const tourRouter = express.Router();
 
 tourRouter.post("/sendTourLocation", authenticateToken, postToursLocation);
 tourRouter.get("/getAllToursData", getAllToursData);
 tourRouter.get("/singleTourData", getSingleTourData);
-// tourRouter.get("/get_limited_data", getLimitedToursData);
 
 export default tourRouter;
